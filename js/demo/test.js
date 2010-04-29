5b98fcdd994abb11322f8af519b982b84c0424cb
Adam Wolff <adam@elasticprocess.com>
2010-04-29 15:06:30 -0700

    Make sure attribute escaping works too

--- js/demo/test.js
+++ js/demo/test.js
@@ -2,4 +2,10 @@ var h1 = document.body.appendChild(document.createElement("h1"));
 h1.appendChild(document.createTextNode("hello sjquery"));
 
 var hidiv = document.body.appendChild(document.createElement("div"));
-hidiv.appendChild(document.createTextNode("本<x'x&s> hi 本<x'x&s>"));
+var encodeMe = "本<x'x&s> hi 本<x'x&s>";
+hidiv.appendChild(document.createTextNode(encodeMe));
+
+var img =  document.body.appendChild(document.createElement("img"))
+img.src ="http://www.appsheriff.com/wp-content/uploads/2010/01/node-js.jpg";
+img.alt = encodeMe;
+img.title = encodeMe;
